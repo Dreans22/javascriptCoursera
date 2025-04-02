4396400b7b269952600224fcaad658bcd275f24d
dreans22 <jaime1w09@gmail.com>
2025-04-02 13:27:00 -0400

    se le agrego la especializacion

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,8 +1,9 @@
 const employees = [
-      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
-      //... Se pueden añadir más registros de empleados aquí
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization:"Javascript" },
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000,specialization:"R" },
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000,specialization:"Python" },
+      { id: 4, name: 'Tayler Johnson', age: 24, department: 'IT', salary: 60000,specialization:"Java" },
+      { id: 5, name: 'Bob Davinson', age: 19, department: 'Finance', salary: 60000,specialization:"Python" },
     ];
 
  // Función para mostrar todos los empleados
@@ -31,3 +32,10 @@ function findEmployeeById(employeeId) {
       document.getElementById('employeesDetails').innerHTML = 'no se ha encontrado ningún empleado con este ID';
 
     }}
+
+function especializacion(){
+    const pythonEs=employees.filter(employee=>employee.specialization=="Python");
+    pythonEs.forEach((employee,index)=>console.log(`el usuario ${index}, se llama ${employee.name}, tiene una especializacion en ${employee.specialization} y gana: ${employee.salary}`))
+    const esfilter = pythonEs.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary} - ${employee.specialization}</p>`).join('');
+     document.getElementById('employeesDetails').innerHTML = esfilter;
+}
