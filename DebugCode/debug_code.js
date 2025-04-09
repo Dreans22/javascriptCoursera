a1a26c3aec93b9560155ab7d6dc77a51ccf09097
dreans22 <jaime1w09@gmail.com>
2025-04-09 08:44:11 -0400

    calculadora

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -1,31 +1,64 @@
-    
-    function performOperation() {
+
+function performOperation() {
     // Get user input from input fields
     let num1 = parseInt(document.getElementById('input1').value);
     let num2 = parseInt(document.getElementById('input2').value);
+    let operacion = document.getElementById('operacion').value;
+    console.log(operacion)
     // Check if inputs are valid numbers
     if (!isNaN(num1) && !isNaN(num2)) {
-    // Perform the operation
-                    let result = multiply(num1, num2);
 
-                    // Display the result
-                    displayResult(result);
-                } else {
-                    displayResult('Please enter valid numbers');
-                }
-            }
+        if (operacion == "sum") {
+            let result = sum(num1, num2);
+            // Display the result
+            displayResult(result);
+        } else if (operacion == "rest") {
+            let result = rest(num1, num2);
+            // Display the result
+            displayResult(result);
+        } else if (operacion == "division") {
+            let result = div(num1, num2);
+            // Display the result
+            displayResult(result);
+        } else {
+            // Perform the operation
+            let result = multiply(num1, num2);
 
-            function multiply(a, b) {
-                // Introduce a debugger statement to pause execution
-                debugger;
+            // Display the result
+            displayResult(result);
+        }
 
-                // Multiply the numbers
-                return a * b;
-            }
+    } else {
+        displayResult('Please enter valid numbers');
+    }
+}
 
-            function displayResult(result) {
-                // Display the result in the paragraph element
-                const resultElement = document.getElementById('result');
-                resultElement.textContent = `The result is: ${result}`;
-            }
-        
+function multiply(a, b) {
+    // Introduce a debugger statement to pause execution
+    debugger;
+
+    // Multiply the numbers
+    return a * b;
+}
+
+function sum(a, b) {
+    debugger;
+    return a + b;
+}
+
+
+function rest(a, b) {
+    debugger;
+    return a - b;
+}
+
+function div(a, b) {
+    debugger;
+    return a / b;
+}
+
+function displayResult(result) {
+    // Display the result in the paragraph element
+    const resultElement = document.getElementById('result');
+    resultElement.textContent = `The result is: ${result}`;
+}
